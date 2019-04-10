d162bffb62f4adb9c3e0d890b9f607e976cd71c2
Benjamin Sykes <33205078+sykeben@users.noreply.github.com>
2019-04-10 11:15:34 -0400

    Made site menu functional.

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,19 @@
 const thisUrl = new URL(window.location);
 const backUrl = thisUrl.searchParams.get("backTo");
 
+const links = document.getElementsByClassName("menu-link")
+
 window.onload = function() {
+
     if (backUrl != null) {
         $('#close-menu').prop("href", backUrl);
+        for (var i=0; i<links.length; i++) {
+            if (links[i].getAttribute("href") == backUrl) {
+                links[i].setAttribute("class", links[i].getAttribute("class") + " active");
+            };
+        };
     } else {
         $('#close-menu').prop("href", "index.html");
-    }
+    };
+
 };
